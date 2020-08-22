cc83e915ed8581d9bdb6f4c99a5f0a7da1a4cd17
Raghav Saxena <35033353+RaghavSaxena96@users.noreply.github.com>
2020-08-22 11:51:33 +0530

    Add files via upload

---
+++ src/main/scala/httpdemo.scala
@@ -0,0 +1,43 @@
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import scala.io.StdIn
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.LoggerOps
+import akka.actor.typed.{ ActorRef, ActorSystem, Behavior }
+
+
+object httpdemo
+{
+  def main(args: Array[String]): Unit = {
+
+    //println("Hi Raghav")
+
+    implicit val system = ActorSystem(Behaviors.empty, "my-system")
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.executionContext
+
+    val route =
+      path("hello") {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+        }
+        post{
+          complete("Post Query")
+        }
+
+      }
+
+    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+    bindingFuture.map(_ => println("Server is online"))
+
+
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+
+
+  }
+}
